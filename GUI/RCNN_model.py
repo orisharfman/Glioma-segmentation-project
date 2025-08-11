2547c447e14ff603e0da0410aa8b736f29963a82
orish <orisharfman@mail.tau.ac.il>
2025-08-11 21:58:35 +0300

    GUI

---
+++ GUI/RCNN_model.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e49c2b59c506a21ae5ca71b9b9ebc8ae9c39046f2c46ce41524b9c8fe6e1b326
+size 4799
---
+++ GUI/segmentation_stats.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6a15bf86304571fdf9fa28fa1956ec38c6a9cd35df76f61ff9205bcba4b2a167
+size 4921
